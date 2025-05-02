555ae4122b93c607983b97ff259897aeaa83e4fe
Niyaz Anuarbek <niyazanuarbek19@gmail.com>
2025-05-02 12:41:26 +0500

    kk

--- src/bot/keyboards.py
+++ src/bot/keyboards.py
@@ -1,8 +1,11 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-def language_keyboard():
+def language_keyboard(show_back=False, language: str = "ru"):
     keyboard = [
         [InlineKeyboardButton("Русский 🇷🇺", callback_data="lang_ru")],
         [InlineKeyboardButton("English 🇬🇧", callback_data="lang_en")]
     ]
+    if show_back:
+        back_label = "🔙 Назад" if language == "ru" else "🔙 Back"
+        keyboard = [[InlineKeyboardButton(back_label, callback_data="lang_back")]]
     return InlineKeyboardMarkup(keyboard)
